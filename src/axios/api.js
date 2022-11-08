029092d37289c1ce0e5bf5d2fb05c60085221866
maryamkaramad <maryamkaramad1373@gmail.com>
2022-11-08 23:11:34 +0330

    change base url in axios

--- src/axios/api.js
+++ src/axios/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "https://78621d56-8cd2-42ff-8df2-fd213f2d9352.mock.pstmn.io/",
+    baseURL: "https://binance43.p.rapidapi.com",
     headers: {
         'X-RapidAPI-Key': '539e4bb6cfmsh6fcf58c502241cdp175c4ejsnedbbc1df4226',
         'X-RapidAPI-Host': 'binance43.p.rapidapi.com'
@@ -9,10 +9,10 @@ const api = axios.create({
 });
 
 export const handelget24hr = () => {
-    return api(`ticker/24hr`).then(data => data.data)
+    return api(`/ticker/24hr`).then(data => data.data)
 }
 
 export const handelgetdataprice = () => {
-    return api(`ticker/price`).then(data => data.data)
+    return api(`/ticker/price`).then(data => data.data)
 }
 
